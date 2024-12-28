22e563d38ded021c0459e78ac51739de37c119ad
kumakoko <sun_of_lover@sina.com>
2024-12-28 18:28:03 +0800

    增加稀疏纹理相关的demo

---
+++ 039_SparseTextureARB/main.cc
@@ -0,0 +1,88 @@
+﻿/**************************************************************************************************************************
+Copyright(C) 2014-2017 www.xionggf.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute,sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the 
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**************************************************************************************************************************/
+
+#include "../klib/kgl_lib_pch.h"
+#include "../klib/kgl_error.h"
+#include "../klib/kgl_string_convertor.h"
+#include "../klib/kgl_message_box.h"
+#include "sparse_texture_arb_app.h"
+
+#if defined(DEBUG) || defined(_DEBUG)
+#pragma comment(lib,"FreeImage.lib")
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glew32.lib")
+#pragma comment(lib,"glfw3dll.lib")
+#pragma comment(lib,"assimp-vc142-mtd.lib")
+#pragma comment(lib,"vld.lib")
+#pragma comment(lib,"libiconv.lib")
+#pragma comment(lib,"freetype.lib")
+#pragma comment(lib,"imgui_d.lib")
+#pragma comment(lib,"klib_d.lib")
+#pragma comment(lib,"legacy_stdio_definitions.lib")
+#else
+#pragma comment(lib,"FreeImage.lib")
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glew32.lib")
+#pragma comment(lib,"glfw3dll.lib")
+#pragma comment(lib,"assimp-vc142-mt.lib")
+#pragma comment(lib,"vld.lib")
+#pragma comment(lib,"libiconv.lib")
+#pragma comment(lib,"freetype.lib")
+#pragma comment(lib,"imgui.lib")
+#pragma comment(lib,"klib.lib")
+#pragma comment(lib,"legacy_stdio_definitions.lib")
+#endif
+
+int main()
+{
+    SparseTextureArbApp* app = nullptr;
+
+    try
+    {
+#if defined(WIN32) || defined(_WIN32)
+        int context_version_major = 3;
+        int context_version_minor = 3;
+        kgl::App::GLProfile profile = kgl::App::CORE;
+#elif defined(__APPLE__) && defined(__MACH__)
+        int context_version_major = 3;
+        int context_version_minor = 3;
+        kgl::App::GLProfile profile = kgl::App::CORE;
+#endif
+        const char* title = "039_SparseTextureARB";
+        app = new SparseTextureArbApp();
+        app->InitWindow(1024, 768, false, title, context_version_major, context_version_minor, profile);
+        app->InitRenderer();
+        app->InitGuiSystem(false);
+        app->InitScene();
+        app->Run();
+    }
+    catch (kgl::Error e)
+    {
+        e.Notify();
+    }
+    catch (std::exception e)
+    {
+        std::wstring exception_desc;
+        kgl::StringConvertor::ANSItoUTF16LE(e.what(), exception_desc);
+        kgl::MessageBox(std::wstring(L"039_SparseTextureARB : Unhandled Exception, aborting"),exception_desc);
+    }
+
+    delete app;
+    
+   
+    return 0;
+}
---
+++ 039_SparseTextureARB/sparse_texture_arb_app.cc
@@ -0,0 +1,99 @@
+﻿/**************************************************************************************************************************
+Copyright(C) 2014-2017 www.xionggf.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute,sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the 
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**************************************************************************************************************************/
+#include "sparse_texture_arb_app.h"
+#include "../klib/kgl_defines.h"
+
+// https://github.com/g-truc/ogl-samples/blob/master/samples/gl-500-texture-sparse-arb.cpp
+
+SparseTextureArbApp::SparseTextureArbApp() :gpu_program_(nullptr), rectangle_primitive_(nullptr),
+triangle_primitive_1_(nullptr), triangle_primitive_2_(nullptr), 
+triangle_primitive_3_(nullptr), triangle_primitive_4_(nullptr)
+{
+}
+
+SparseTextureArbApp::~SparseTextureArbApp()
+{
+    KGL_SAFE_DELETE(gpu_program_);
+    KGL_SAFE_DELETE(rectangle_primitive_);
+    KGL_SAFE_DELETE(triangle_primitive_3_);
+    KGL_SAFE_DELETE(triangle_primitive_2_);
+    KGL_SAFE_DELETE(triangle_primitive_3_);
+    KGL_SAFE_DELETE(triangle_primitive_4_);
+}
+
+void SparseTextureArbApp::InitModel()
+{
+    gpu_program_ = new kgl::GPUProgram;
+    const GLchar* vs_file_path = nullptr;
+    const GLchar* fs_file_path = nullptr;
+
+#if defined(WIN32) || defined(_WIN32)
+    vs_file_path = "resources/shader/039_sparse_texture_arb_vs.glsl";
+    fs_file_path = "resources/shader/039_sparse_texture_arb_fs.glsl";
+#endif
+    gpu_program_->CreateFromFile(vs_file_path, fs_file_path, nullptr);
+
+
+    GLfloat vertices[] = 
+    {
+        // 位置             // 颜色
+        -1.0f,-1.0f, 0.0f, 1.0f,
+         1.0f,-1.0f, 1.0f, 1.0f,
+         1.0f, 1.0f, 1.0f, 0.0f,
+        -1.0f, 1.0f, 0.0f, 0.0f
+    };
+
+    GLuint indices[] =
+    {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    kgl::VertexAttribute va_position;
+    va_position.index = 0;
+    va_position.normalized = GL_FALSE;
+    va_position.type = GL_FLOAT;
+    va_position.size = 2;                       // 一个“顶点位置”的属性由2个分量组成
+    va_position.stride = 4 * sizeof(GLfloat);   // 每个顶点的步长为4个float
+    va_position.pointer = nullptr;
+    
+    kgl::VertexAttribute va_texcoord;
+    va_texcoord.index = 1;
+    va_texcoord.normalized = GL_FALSE;
+    va_texcoord.type = GL_FLOAT;
+    va_texcoord.size = 2; // 一个“顶点纹理坐标”的属性由2个分量组成
+    va_texcoord.stride = 4 * sizeof(GLfloat); // 每个位置的步长为 
+    va_texcoord.pointer = reinterpret_cast<GLvoid*> (2 * sizeof(GLfloat));
+
+    std::vector<kgl::VertexAttribute> vtx_attri_array;
+    vtx_attri_array.push_back(va_position);
+    vtx_attri_array.push_back(va_texcoord);
+
+    rectangle_primitive_ = new kgl::Primitive;
+    rectangle_primitive_->CreateIndexed(GL_TRIANGLES, vertices, sizeof(vertices), GL_STATIC_DRAW, kgl::Primitive::UINT32, indices, sizeof(indices), GL_STATIC_DRAW, vtx_attri_array);
+}
+
+void SparseTextureArbApp::RenderScene()
+{
+    time = glfwGetTime();
+    /*gpu_program_->Use();
+    rectangle_primitive_->DrawIndexed();
+    triangle_primitive_1_->DrawIndexed();
+    triangle_primitive_2_->DrawIndexed();
+    triangle_primitive_3_->DrawIndexed();
+    triangle_primitive_4_->DrawIndexed();*/
+}
